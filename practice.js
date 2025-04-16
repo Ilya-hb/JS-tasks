7268f87a0026b5faa0902b45ec8cc721baa80340
ilyholu <ilyholu@gmail.com>
2025-04-16 21:50:06 +0300

    55

--- practice.js
+++ practice.js
@@ -1,7 +1,15 @@
-function reverseWords(str) {
-  const result = str.split(" ").map((el) => el.split("").reverse().join("")).join(" ");
+const getData = (url) =>
+  new Promise((resolve, reject) =>
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => resolve(json))
+      .catch((error) => reject(error))
+  );
 
-  console.log(result);
-}
+console.log(
+  getData("https://jsonplaceholder.typicode.com/todos")
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error))
+);
 
-reverseWords("The quick brown fox jumps over the lazy dog.");
+// switch to async/await
