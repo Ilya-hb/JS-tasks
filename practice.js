4ff1d714ff39d82466bc5d2a70da0d740f84c36e
Ilya <ilyholu@gmail.com>
2025-05-07 09:00:33 +0300

    Leetcode(Easy)#1920-Array-permutation

--- practice.js
+++ practice.js
@@ -1,23 +1,9 @@
-function likes(names) {
-  if (!Array.isArray(names)) return "Invalid input";
-  switch (names.length) {
-    case 0:
-      return "no one likes this";
-    case 1:
-      return `${names[0]} likes this`;
-    case 2:
-      return `${names[0]} and ${names[1]} likes this`;
-    case 3:
-      return `${names[0]}, ${names[1]} and ${names[3]} like this`;
-    default:
-      return `${names[0]}, ${names[1]} and ${names.length - 2} other like this`;
-  }
-}
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function (nums) {
+  return nums.map((el, index) => nums[nums[index]]);
+};
 
-console.log(likes([])); // "no one likes this"
-console.log(likes(["Peter"])); // "Peter likes this"
-console.log(likes(["Jacob", "Alex"])); // "Jacob and Alex like this"
-console.log(likes(["Max", "John", "Mark"])); // "Max, John and Mark like this"
-console.log(likes(["Alex", "Jacob", "Mark", "Max"])); // "Alex, Jacob and 2 others like this"
-console.log(likes(["A", "B", "C", "D", "E"])); // "A, B and 3 others like this"
-console.log(likes("Peter")); // "Invalid input"
+console.log(buildArray([5, 0, 1, 2, 3, 4]));
