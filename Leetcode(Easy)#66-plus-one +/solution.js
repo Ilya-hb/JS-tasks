936894e64f4448c6c508602c9a7c5840b92b6774
ilyholu <ilyholu@gmail.com>
2025-05-26 22:20:56 +0300

    Leetcode(Easy)#66-plus-one

--- Leetcode(Easy)#66-plus-one +/solution.js
+++ Leetcode(Easy)#66-plus-one +/solution.js
@@ -4,10 +4,7 @@
  */
 var plusOne = function (digits) {
   let strDigits = digits.toString().split(",").join("");
-  const result = (parseInt(strDigits) + 1).toString().split("");
+  const result = (BigInt(strDigits) + 1n).toString().split("");
 
   return result.map((el) => +el);
 };
-
-console.log(plusOne([1, 2, 3]));
-console.log(plusOne([9]));
