0d49079bdb4779ecf6a63f2e7e8a286118c5711c
ilyholu <ilyholu@gmail.com>
2025-08-25 14:23:25 +0300

    Leetcode(Easy)#217-Contains-duplicate

--- practice.js
+++ practice.js
@@ -1,36 +1,20 @@
 /**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number}
+ * @param {number[]} nums
+ * @return {boolean}
  */
-
-// the overall time complexity should be O(log (m+n)) but my has O((m+n) log (m+n)) because of sort method
-var findMedianSortedArrays = function (nums1, nums2) {
-  // My approach
-  const resultArr = nums1.concat(nums2).sort((a, b) => a - b); // bad optimization
-  const length = resultArr.length;
-  console.log(resultArr);
-  if (length % 2 !== 0) {
-    return resultArr.at(length / 2);
-  } else {
-    let leftMedIndex = resultArr.at(Math.round((length - 1) / 2));
-    let rightMedIndex = resultArr.at(Math.round((length - 1.1) / 2));
-    return (leftMedIndex + rightMedIndex) / 2;
+var containsDuplicate = function (nums) {
+  const uniqueNums = Array.from(new Set(nums));
+  const resultObj = {};
+  for (num of uniqueNums) {
+    resultObj[num] = 0;
   }
-
-  // Better variant
-  //   const a = nums1.concat(nums2).sort((x, y) => x - y);
-  // const n = a.length;
-  // if (n % 2) return a[Math.floor(n / 2)];
-  // const mid = n / 2;
-  // return (a[mid - 1] + a[mid]) / 2;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] in resultObj) {
+      resultObj[nums[i]]++;
+    }
+  }
+  console.log(resultObj);
+  return Object.values(resultObj).some((el) => el >= 2);
 };
 
-// console.log(findMedianSortedArrays([1, 2], [3, 4])); //length 4
-// console.log(findMedianSortedArrays([1, 3], [2])); //length 3
-console.log(
-  findMedianSortedArrays(
-    [1, 2, 3, 4, 5],
-    [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
-  )
-);
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
