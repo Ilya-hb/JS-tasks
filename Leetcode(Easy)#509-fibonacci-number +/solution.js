0b3e67383838873b9b0ee089539346f1ceff9cbb
ilyholu <ilyholu@gmail.com>
2025-08-27 10:46:11 +0300

    fibonacci solution code

---
+++ Leetcode(Easy)#509-fibonacci-number +/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// time complexity O(2^n), my solution
+// var fib = function (n) {
+//   if (n > 1) {
+//     return fib(n - 1) + fib(n - 2);
+//   }
+//   return n;
+// };
+
+// console.log(fib(3));
+
+//better approach using memoization O(n)
+var fib = function (n, memo = {}) {
+  if (n <= 1) return n;
+  if (n in memo) return memo[n]; //if already exists
+  console.log(memo);
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo); //caching result
+  return memo[n];
+};
+
+console.log(fib(4));
